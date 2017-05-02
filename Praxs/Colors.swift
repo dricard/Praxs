3930aa9c9bf0d51add2b2d49e9c4ee853b044831
Denis Ricard <d.ricard@me.com>
2017-05-02 11:49:29 -0400

    doc: add documentation to Colors

--- Praxs/Colors.swift
+++ Praxs/Colors.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+/// Theme colors. Contains two arrays, one for contexts' background colors,
+/// and one for contexts' text color. These are also used for the top handles
+/// in the contextViews.
+/// The `night` parameter is the index to use for the background (night) color.
 struct Colors {
     static let night = 2
     static let context: [UIColor] = [PraxsStyleKit.context_01_color, PraxsStyleKit.context_02_color, PraxsStyleKit.context_03_color, PraxsStyleKit.context_04_color, PraxsStyleKit.context_05_color]
